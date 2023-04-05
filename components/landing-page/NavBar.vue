52149519f341812a5ea66c45999b14419fc7b4cf
akb4r5yn <akbar5yn.developer@gmail.com>
2023-04-05 12:44:39 +0700

    created event modal on button login and register

--- components/landing-page/NavBar.vue
+++ components/landing-page/NavBar.vue
@@ -3,48 +3,87 @@
     <nav
       class="p-[19px] flex justify-between text-center items-center border-2"
     >
+      <!-- nav-logo -->
       <div class="flex gap-4 items-center">
         <img src="../../assets/img/logo.svg" alt="logo" class="w-[70px]">
         <h1 class="text-4xl font-medium">
           Opibox
         </h1>
       </div>
+
+      <!-- nav-item -->
       <ul class="flex gap-10">
         <li>
-          <NuxtLink to="#">
+          <nuxt-link to="#">
             Beranda
-          </NuxtLink>
+          </nuxt-link>
         </li>
         <li>
-          <NuxtLink to="#">
+          <nuxt-link to="#">
             Layanan
-          </NuxtLink>
+          </nuxt-link>
         </li>
         <li>
-          <NuxtLink to="#">
+          <nuxt-link to="#">
             Program Loyaliti
-          </NuxtLink>
+          </nuxt-link>
         </li>
         <li>
-          <NuxtLink to="#">
+          <nuxt-link to="#">
             Bantuan
-          </NuxtLink>
+          </nuxt-link>
         </li>
       </ul>
+
+      <!-- login-register -->
       <div class="gap-1">
-        <button class="border-black border-[1px] px-7 py-2 rounded-[6px]">
+        <!-- btn-login -->
+        <button
+          class="border-black border-[1px] px-7 py-2 rounded-[6px]"
+          @click="modalLogin = true"
+        >
           Masuk
         </button>
+
+        <!-- btn-register -->
         <button
           class="bg-[#4A4A4F] border-[#4A4A4F] border-[1px] px-7 py-2 rounded-[6px] text-white"
+          @click="modalRegister = true"
         >
           Daftar
         </button>
+
+        <!-- show modal login -->
+        <auth-component-modal v-if="modalLogin" class="">
+          <button @click="modalLogin = false">
+            <span>x</span>
+          </button>
+          <h1>Masuk</h1>
+        </auth-component-modal>
+
+        <!-- show modal register -->
+        <auth-component-modal v-if="modalRegister">
+          <button @click="modalRegister = false">
+            <span>x</span>
+          </button>
+          <h1>Daftar</h1>
+        </auth-component-modal>
       </div>
     </nav>
   </div>
 </template>
 
-<script></script>
+<script>
+export default {
+  name: 'NavBar',
+
+  data () {
+    return {
+      modalLogin: false,
+      modalRegister: false
+    }
+  }
+}
+</script>
 
 <style></style>
