027850adf697a275fe890b86a05a5c9dfe6a3aec
akb4r5yn <akbar5yn.developer@gmail.com>
2023-04-08 23:25:20 +0700

    added layout for auth

---
+++ layouts/AuthView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div class="flex justify-center border border-b-2 py-11 fixed w-full z-50">
+      <nuxt-link to="/">
+        <img src="../assets/img/logo.svg" class="w-[45px]" alt="logo">
+      </nuxt-link>
+    </div>
+    <div class="flex h-screen justify-center items-center">
+      <nuxt />
+    </div>
+  </div>
+</template>
+
+<script></script>
+
+<style></style>
