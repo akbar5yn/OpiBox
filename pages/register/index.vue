9be0d10540d5d359939d24c24a9b43007ee6f2c5
akb4r5yn <akbar5yn.developer@gmail.com>
2023-04-09 21:52:55 +0700

    created password visibilty done

--- pages/register/index.vue
+++ pages/register/index.vue
@@ -10,7 +10,7 @@
       <label for="">Nama</label>
       <input
         id="nama"
-        class="border rounded-lg px-2 py-3 text-[14px]"
+        class="border rounded-lg px-2 py-3 text-[14px] outline-none"
         type="text"
         name="nama"
         placeholder="Masukan nama"
@@ -19,29 +19,61 @@
       <label for="">Email</label>
       <input
         id="email"
-        class="border rounded-lg px-2 py-3 text-[14px]"
+        class="border rounded-lg px-2 py-3 text-[14px] outline-none"
         type="text"
         name="email"
         placeholder="Masukan email"
       >
 
       <label for="">Kata sandi</label>
-      <input
-        id="password"
-        class="border rounded-lg px-2 py-3 text-[14px]"
-        type="password"
-        name="password"
-        placeholder="Masukan kata sandi"
-      >
+      <div class="password-input-wrapper flex relative border rounded-lg">
+        <input
+          id="password"
+          class="px-2 py-3 w-[90%] text-[14px] outline-none"
+          name="password"
+          placeholder="Masukan kata sandi"
+          :type="showPassword ? 'text' : 'password'"
+        >
+        <img
+          v-if="showPassword"
+          src="../../assets/img/eye-slash.svg"
+          alt="eye"
+          class="absolute cursor-pointer self-center flex right-4"
+          @click="showPassword = !showPassword"
+        >
+        <img
+          v-if="!showPassword"
+          src="../../assets/img/eye.svg"
+          alt="eye"
+          class="absolute cursor-pointer self-center flex right-4"
+          @click="showPassword = !showPassword"
+        >
+      </div>
 
       <label for="">Konfirmasi kata sandi</label>
-      <input
-        id="password"
-        class="border rounded-lg px-2 py-3 text-[14px]"
-        type="password"
-        name="password"
-        placeholder="Masukan kata sandi"
-      >
+      <div class="password-input-wrapper flex relative border rounded-lg">
+        <input
+          id="password"
+          class="px-2 py-3 w-[90%] text-[14px] outline-none"
+          name="password"
+          placeholder="Masukan kata sandi"
+          :type="showPasswordConfirmed ? 'text' : 'password'"
+        >
+        <img
+          v-if="showPasswordConfirmed"
+          src="../../assets/img/eye-slash.svg"
+          alt="eye"
+          class="absolute cursor-pointer self-center flex right-4"
+          @click="showPasswordConfirmed = !showPasswordConfirmed"
+        >
+        <img
+          v-if="!showPasswordConfirmed"
+          src="../../assets/img/eye.svg"
+          alt="eye"
+          class="absolute cursor-pointer self-center flex right-4"
+          @click="showPasswordConfirmed = !showPasswordConfirmed"
+        >
+      </div>
 
       <div class="flex justify-end underline">
         <nuxt-link to="" class="mt-2">
@@ -68,6 +100,14 @@
 <script>
 export default {
   name: 'RegisterView',
-  layout: 'AuthView'
+  layout: 'AuthView',
+
+  data () {
+    return {
+      showPassword: false,
+      showPasswordConfirmed: false,
+      isClose: false
+    }
+  }
 }
 </script>
