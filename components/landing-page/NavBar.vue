755fbdf2d18c6a2026419275e18b2a3c1c03b038
akb4r5yn <akbar5yn.developer@gmail.com>
2023-04-10 15:53:18 +0700

    make a responsive navbar on mobile

--- components/landing-page/NavBar.vue
+++ components/landing-page/NavBar.vue
@@ -5,14 +5,18 @@
     >
       <!-- nav-logo -->
       <div class="flex gap-4 items-center">
-        <img src="../../assets/img/logo.svg" alt="logo" class="w-[70px]">
-        <h1 class="text-4xl font-medium">
+        <img
+          src="../../assets/img/logo.svg"
+          alt="logo"
+          class="w-10 sm:w-[70px]"
+        >
+        <h1 class="text-4xl font-medium hidden">
           Opibox
         </h1>
       </div>
 
       <!-- nav-item -->
-      <ul class="flex gap-10">
+      <ul class="hidden md:flex gap-10">
         <li>
           <nuxt-link to="#">
             Beranda
@@ -40,7 +44,7 @@
         <!-- btn-login -->
         <nuxt-link
           to="/login"
-          class="border-black border-[1px] px-7 py-2 rounded-[6px]"
+          class="hidden md:block border-black border-[1px] px-7 py-2 rounded-[6px]"
         >
           Masuk
         </nuxt-link>
@@ -48,10 +52,15 @@
         <!-- btn-register -->
         <nuxt-link
           to="/register"
-          class="bg-[#4A4A4F] border-[#4A4A4F] border-[1px] px-7 py-2 rounded-[6px] text-white"
+          class="hidden md:block bg-[#4A4A4F] border-[#4A4A4F] border-[1px] px-7 py-2 rounded-[6px] text-white"
         >
           Daftar
         </nuxt-link>
+      </div>
+
+      <!-- humberger menu -->
+      <div class="sm:hidden">
+        Menu
       </div>
     </nav>
   </div>
