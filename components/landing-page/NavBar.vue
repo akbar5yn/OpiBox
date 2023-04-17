d54676f7c26e045463593acff6ca93679eddf8ad
akb4r5yn <akbar5yn.developer@gmail.com>
2023-04-17 20:43:06 +0700

    fixed typo on homepage id

--- components/landing-page/NavBar.vue
+++ components/landing-page/NavBar.vue
@@ -65,7 +65,7 @@
           >
             <li>
               <nuxt-link
-                to="#"
+                to="#beranda"
                 class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
                 aria-current="page"
               >
@@ -74,7 +74,7 @@
             </li>
             <li>
               <nuxt-link
-                to="#"
+                to="#layanan"
                 class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >
                 Layanan
@@ -82,7 +82,7 @@
             </li>
             <li>
               <nuxt-link
-                to="#"
+                to="#loyality"
                 class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >
                 Program Loyaliti
@@ -90,7 +90,7 @@
             </li>
             <li>
               <nuxt-link
-                to="#"
+                to="#bantuan"
                 class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >
                 Bantuan
