64a74b92b67fc344972b09ad89b2190743be127b
akb4r5yn <akbar5yn.developer@gmail.com>
2023-04-17 22:48:18 +0700

    Remove some unneeded classes

--- components/landing-page/NavBar.vue
+++ components/landing-page/NavBar.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <nav
-      class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600"
+      class="bg-white fixed w-full z-20 top-0 left-0 border-b border-gray-200"
     >
       <div
         class="w-full flex flex-wrap items-center justify-between mx-auto p-4"
@@ -37,7 +37,7 @@
           <button
             data-collapse-toggle="navbar-sticky"
             type="button"
-            class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
             aria-controls="navbar-sticky"
             aria-expanded="false"
           >
@@ -63,13 +63,13 @@
           class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
         >
           <ul
-            class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
+            class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white"
           >
             <li>
               <nuxt-link
                 to="#beranda"
-                class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
-                aria-current="page"
+                class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:p-0"
+                active-class="border-b-2 border-[#6C61E1]"
               >
                 Beranda
               </nuxt-link>
@@ -77,7 +77,8 @@
             <li>
               <nuxt-link
                 to="#layanan"
-                class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:p-0"
+                active-class="border-b-2 border-[#6C61E1]"
               >
                 Layanan
               </nuxt-link>
@@ -85,7 +86,8 @@
             <li>
               <nuxt-link
                 to="#loyality"
-                class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:p-0"
+                active-class="border-b-2 border-[#6C61E1]"
               >
                 Program Loyaliti
               </nuxt-link>
@@ -93,7 +95,8 @@
             <li>
               <nuxt-link
                 to="#bantuan"
-                class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:p-0"
+                active-class="border-b-2 border-[#6C61E1]"
               >
                 Bantuan
               </nuxt-link>
