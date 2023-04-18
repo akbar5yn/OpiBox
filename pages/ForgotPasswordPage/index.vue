ac83b58b5389dbe2c2e2413554e5b4301b0b161b
Rinta Sari <rintasari64@gmail.com>
2023-04-18 12:06:47 +0700

    Create a title (Forgot password) in forgot password page section

--- pages/ForgotPasswordPage/index.vue
+++ pages/ForgotPasswordPage/index.vue
@@ -1,7 +1,9 @@
 <template>
-    <div>
-        
-    </div>
+  <div class="h-[620px]">
+    <h1 class="text-[50px] font-cabinet-grotesk font-normal">
+      Lupa Kata Sandi
+    </h1>
+  </div>
 </template>
 
 <script>
@@ -10,11 +12,22 @@ export default {
   layout: 'AuthView',
 
   data () {
-        return {
-            showPassword: false,
-            showPasswordConfirmed: false,
-            isClose: false
-        }
+    return {
+      showPassword: false,
+      showPasswordConfirmed: false,
+      isClose: false
     }
+  }
 }
 </script>
+
+<style scoped>
+@font-face {
+  font-family: 'Cabinet Grotesk';
+  src: url('@/assets/CabinetGrotesk-Medium.otf') format('truetype');
+}
+
+.font-cabinet-grotesk {
+  font-family: 'Cabinet Grotesk', sans-serif;
+}
+</style>
