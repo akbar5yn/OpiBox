6c7420ba171a064dd1e3b83758ba97f6a8ba2607
Rinta Sari <rintasari64@gmail.com>
2023-04-18 13:54:55 +0700

    Create a section description of the forgot password page

--- pages/ForgotPasswordPage/index.vue
+++ pages/ForgotPasswordPage/index.vue
@@ -1,8 +1,19 @@
 <template>
-  <div class="h-[620px]">
-    <h1 class="text-[50px] font-cabinet-grotesk font-normal">
-      Lupa Kata Sandi
-    </h1>
+  <div class="h-[650px] items-center">
+    <div class="text-center">
+      <h1 class="text-[50px] font-cabinet-grotesk font-normal text-[#19191B]">
+        Lupa Kata Sandi
+      </h1>
+      <p
+        class="text-[20px] font-normal font-open-sans order-1 text-[#4A4A4F]"
+        style="line-height: 2"
+      >
+        Masukkan <strong> alamat email </strong> yang Anda gunakan untuk
+        mendaftar <br>
+        akun Opibox. Kami akan mengirim pesan melalui email berisi <br>
+        tautan untuk mereset kata sandi Anda.
+      </p>
+    </div>
   </div>
 </template>
 
@@ -29,5 +40,14 @@ export default {
 
 .font-cabinet-grotesk {
   font-family: 'Cabinet Grotesk', sans-serif;
+}
+
+@font-face {
+  font-family: 'Open Sans';
+  src: url('@/assets/OpenSans-Light.ttf') format('truetype');
+}
+
+.font-open-sans {
+  font-family: 'Open Sans', sans-serif;
 }
 </style>
