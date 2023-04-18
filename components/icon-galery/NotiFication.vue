956785726687f0e6a14cd4c964fea39d4a919464
akb4r5yn <akbar5yn.developer@gmail.com>
2023-04-18 13:52:17 +0700

    create icon notification component for use in dashboard layout

--- components/icon-galery/NotiFication.vue
+++ components/icon-galery/NotiFication.vue
@@ -1,8 +1,7 @@
 <template>
   <div>
     <svg
-      width="28"
-      height="30"
+      class="w-7"
       viewBox="0 0 28 30"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
@@ -17,3 +16,9 @@
     </svg>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'NotiFication'
+}
+</script>
