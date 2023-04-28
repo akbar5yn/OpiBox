1cae568922a67395a150b98a5eda1a635ebeaef6
Rinta Sari <rintasari64@gmail.com>
2023-04-28 22:28:45 +0700

    create login form validation

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -6,21 +6,31 @@
     <p class="text-[14px] font-normal tracking-wider text-[#8B8B8B]">
       Masuk dan nikmati semua layanan di Opibox
     </p>
-    <form action="" class="flex flex-col gap-y-3 mt-4">
+
+    <p v-if="errorsInput.status" class="text-red-500 text-sm">
+      {{ errorsInput.message }}
+    </p>
+
+    <form
+      action=""
+      class="flex flex-col gap-y-3 mt-4"
+      @submit="submitData($event)"
+    >
       <label for="">Email</label>
       <input
         id="email"
+        v-model="form.email"
         class="border rounded-lg px-2 py-3 text-[14px] outline-none"
         type="text"
         name="email"
         placeholder="Masukan email"
-        required
       >
 
       <label for="">Kata sandi</label>
       <div class="password-input-wrapper flex relative border rounded-lg">
         <input
           id="password"
+          v-model="form.password"
           class="px-2 py-3 text-[14px] w-[90%] outline-none"
           name="password"
           placeholder="Masukan kata sandi"
@@ -71,8 +81,41 @@ export default {
 
   data () {
     return {
+      form: {
+        email: '',
+        password: ''
+      },
+      disabled: true,
+      errorsInput: {
+        message: 'Tess error',
+        status: false
+      },
       showPassword: false,
       isClose: false
+    }
+  },
+  watch: {
+    form: {
+      deep: true,
+      handler () {
+        this.checkInput()
+      }
+    }
+  },
+  methods: {
+    checkInput () {
+      this.disabled = !Object.keys(this.form).every(e => this.form[e] !== '')
+    },
+    submitData (e) {
+      e.preventDefault()
+      if (this.form.email === '' || this.form.password === '') {
+        this.errorsInput.status = true
+        this.errorsInput.message = 'Email dan kata sandi harus diisi!'
+      }
+
+      if (this.errorsInput.status) {
+        return false
+      }
     }
   }
 }
