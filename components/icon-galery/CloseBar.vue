a80246b3957aeac84d843d6110ca00312467e32d
akb4r5yn <akbar5yn.developer@gmail.com>
2023-04-30 06:10:28 +0700

    create closebar icon component

---
+++ components/icon-galery/CloseBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <button class="" @click="$emit('toggle-menu', { sideBar: !isClose })">
+    <!-- <button class="" @click="closeBar"> -->
+    <svg class="w-5" viewBox="0 0 14 14" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M1 13L13 1M1 1L13 13"
+        stroke="#151B1E"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'CloseBar',
+
+  props: {
+    isClose: Boolean
+  }
+}
+</script>
