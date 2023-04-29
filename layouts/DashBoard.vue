174dc090724f50dc0f1c18a5895d709049b682fa
akb4r5yn <akbar5yn.developer@gmail.com>
2023-04-30 06:25:47 +0700

    added style class in dashboard to sort content

--- layouts/DashBoard.vue
+++ layouts/DashBoard.vue
@@ -1,10 +1,17 @@
 <template>
-  <div>
-    <!-- topbar -->
-    <dashboard-top-bar />
+  <div class="flex">
+    <div class="order-2 flex flex-col w-full">
+      <!-- topbar -->
+      <dashboard-top-bar />
+
+      <!-- main content -->
+      <h1 id="#dash" class="flex justify-center items-center h-full">
+        Main Content
+      </h1>
+    </div>
 
     <!-- sidebar -->
-    <dashboard-side-bar />
+    <dashboard-side-bar class="order-1" />
   </div>
 </template>
 
