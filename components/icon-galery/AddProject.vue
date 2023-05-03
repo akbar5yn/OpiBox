72d5c970105b537d7211749dced93cd94928f44f
akb4r5yn <akbar5yn.developer@gmail.com>
2023-05-04 05:09:32 +0700

    create props to capture data from parent

--- components/icon-galery/AddProject.vue
+++ components/icon-galery/AddProject.vue
@@ -1,14 +1,14 @@
 <template>
   <svg
-    width="36"
-    height="36"
+    :width="width"
+    :height="height"
     viewBox="0 0 36 36"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
+    :stroke="strokeColor"
   >
     <path
       d="M18 2.6665V33.3332M33.3334 17.9998L2.66669 17.9998"
-      stroke="white"
       stroke-width="3.83333"
       stroke-linecap="round"
       stroke-linejoin="round"
@@ -18,6 +18,20 @@
 
 <script>
 export default {
-  name: 'AddProject'
+  name: 'AddProject',
+  props: {
+    strokeColor: {
+      type: String,
+      default: 'white'
+    },
+    width: {
+      type: String,
+      default: '36'
+    },
+    height: {
+      type: String,
+      default: '36'
+    }
+  }
 }
 </script>
