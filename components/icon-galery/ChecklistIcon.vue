b4684947f9de1b129fe4ab8ffe830aaf0141341b
akb4r5yn <akbar5yn.developer@gmail.com>
2023-05-04 05:05:43 +0700

    create a component icon for the create project page

---
+++ components/icon-galery/ChecklistIcon.vue
@@ -0,0 +1,23 @@
+<template>
+  <svg
+    width="19"
+    height="15"
+    viewBox="0 0 19 15"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M1.69751 8.65916L6.33617 13.2978L17.9328 1.70117"
+      stroke="white"
+      stroke-width="1.54622"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'ChecklistIcon'
+}
+</script>
---
+++ components/icon-galery/ListIcon.vue
@@ -0,0 +1,22 @@
+<template>
+  <svg
+    width="16"
+    height="21"
+    viewBox="0 0 16 21"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M5 3.5H3C1.89543 3.5 1 4.39543 1 5.5V17.5C1 18.6046 1.89543 19.5 3 19.5H13C14.1046 19.5 15 18.6046 15 17.5V5.5C15 4.39543 14.1046 3.5 13 3.5H11M5 3.5C5 4.60457 5.89543 5.5 7 5.5H9C10.1046 5.5 11 4.60457 11 3.5M5 3.5C5 2.39543 5.89543 1.5 7 1.5H9C10.1046 1.5 11 2.39543 11 3.5M8 10.5H11M8 14.5H11M5 10.5H5.01M5 14.5H5.01"
+      stroke="#19191B"
+      stroke-width="2"
+      stroke-linecap="round"
+    />
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'ListIcon'
+}
+</script>
