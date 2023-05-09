30d4acccd60fabc5de0ebac8d965e08bd677afcf
Rinta Sari <rintasari64@gmail.com>
2023-05-09 23:27:29 +0700

    implement reset password API

--- store/password.js
+++ store/password.js
@@ -5,5 +5,15 @@ export const actions = {
     } catch (err) {
       return err.response
     }
+  },
+  async updatePassword (state, payload) {
+    try {
+      const data = {
+        password: payload.password
+      }
+      return await this.$axios.$patch(`update_password/${payload.token}`, data)
+    } catch (err) {
+      return err.response
+    }
   }
 }
