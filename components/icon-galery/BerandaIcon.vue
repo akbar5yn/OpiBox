c77cdea229f247ed3e036bc28896fdecc21a74fc
akb4r5yn <akbar5yn.developer@gmail.com>
2023-05-11 00:10:06 +0700

    create an icon component from the icon that has been added

---
+++ components/icon-galery/BerandaIcon.vue
@@ -0,0 +1,23 @@
+<template>
+  <svg
+    width="20"
+    height="21"
+    viewBox="0 0 20 21"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M1 10.168L3 8.16797M3 8.16797L10 1.16797L17 8.16797M3 8.16797V18.168C3 18.7203 3.44772 19.168 4 19.168H7M17 8.16797L19 10.168M17 8.16797V18.168C17 18.7203 16.5523 19.168 16 19.168H13M7 19.168C7.55228 19.168 8 18.7203 8 18.168V14.168C8 13.6157 8.44772 13.168 9 13.168H11C11.5523 13.168 12 13.6157 12 14.168V18.168C12 18.7203 12.4477 19.168 13 19.168M7 19.168H13"
+      stroke="#19191B"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'BerandaIcon'
+}
+</script>
---
+++ components/icon-galery/MyProfile.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <svg
+      width="16"
+      height="21"
+      viewBox="0 0 16 21"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M12 5.66797C12 7.87711 10.2091 9.66797 8 9.66797C5.79086 9.66797 4 7.87711 4 5.66797C4 3.45883 5.79086 1.66797 8 1.66797C10.2091 1.66797 12 3.45883 12 5.66797Z"
+        stroke="#19191B"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M8 12.668C4.13401 12.668 1 15.802 1 19.668H15C15 15.802 11.866 12.668 8 12.668Z"
+        stroke="#19191B"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyProfile'
+}
+</script>
