fcf4c40a0a48f664a6a3174e63bd43a195f18d6a
akb4r5yn <akbar5yn.developer@gmail.com>
2023-05-13 13:25:16 +0700

    added layout default view for main page

---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="flex">
+    <div class="order-2 w-full">
+      <Nuxt />
+    </div>
+
+    <!-- sidebar -->
+    <mainpage-side-bar class="order-1" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Default',
+  computed: {
+    pageTitle () {
+      // Mengambil nama halaman dari objek route
+      return this.$route.name
+    }
+  }
+}
+</script>
