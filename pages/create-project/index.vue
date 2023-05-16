190e8c2ad11c4cac823782905837cd868abdfd61
akb4r5yn <akbar5yn.developer@gmail.com>
2023-05-16 14:12:39 +0700

    added page for create project

---
+++ pages/create-project/index.vue
@@ -0,0 +1,240 @@
+<template>
+  <div id="create-project">
+    <form class="w-full" @submit.prevent="">
+      <!-- topbar -->
+      <section
+        class="py-[10px] px-[28px] border-b-2 border-[#CACACE] flex justify-between items-center fixed w-full"
+      >
+        <!-- icon opibox -->
+        <NuxtLink to="/beranda">
+          <icon-galery-opi-box />
+        </NuxtLink>
+        <!-- icon else -->
+        <div class="flex items-center">
+          <button
+            class="bg-[#6C61E1] text-white px-[18px] py-[8px] rounded-lg flex items-center gap-[18px]"
+            :class="{
+              'bg-[#E5E5E6] cursor-not-allowed text-[#B0B0B5]': !isFormValid
+            }"
+            type="submit"
+            :disabled="!isFormValid"
+            @click="simpan"
+          >
+            <icon-galery-checklist-icon />
+            Simpan
+          </button>
+        </div>
+      </section>
+
+      <!-- main form -->
+      <section>
+        <div class="flex h-screen">
+          <!-- image section -->
+          <div class="p-9 px-20 mt-16 border-r w-1/2 relative">
+            <!-- image preview hidden -->
+            <div
+              v-if="!showPreview"
+              class="border-2 h-full border-dashed border-[#95959D] py-4 flex flex-col items-center justify-center gap-y-2"
+            >
+              <icon-galery-image-icon />
+              <p class="text-[#95959D] text-base xl:text-3xl text-center">
+                Anda belum mengunggah gambar
+              </p>
+            </div>
+
+            <!-- image preview muncul-->
+            <div
+              v-if="showPreview"
+              id="image-preview"
+              class="h-full border-dashed border-[#95959D] flex flex-col items-center justify-center"
+            >
+              <button
+                v-if="currentImageIndex !== 0"
+                class="slider-button absolute top-1/2 left-3 rounded-full shadow-xl p-4"
+                @click="prevImage"
+              >
+                <icon-galery-slider-icon class="rotate-180" />
+              </button>
+              <img
+                :src="selectedImg[currentImageIndex]"
+                alt="Image preview"
+                class="object-contain w-full h-[100%]"
+              >
+              <button
+                v-if="selectedImg.length > 1"
+                class="slider-button absolute top-1/2 right-3 rounded-full shadow-xl p-4"
+                @click="nextImage"
+              >
+                <icon-galery-slider-icon />
+              </button>
+            </div>
+          </div>
+
+          <!-- form section -->
+          <div class="p-9 mt-16 border-r w-1/2">
+            <!-- btn akses & add gambar -->
+            <div class="flex justify-between">
+              <button
+                class="flex items-center bg-[#6C61E1] px-5 py-3 gap-3 rounded-lg"
+                @click="showAkses"
+              >
+                <icon-galery-setting-icon color="white" />
+                <span class="text-white font-cabinet-grotesk text-lg">Kelola Akses</span>
+              </button>
+              <label
+                for="addImg"
+                class="flex items-center bg-[#6C61E1] px-5 py-3 gap-3 rounded-lg cursor-pointer"
+              >
+                <icon-galery-add-team width="30" />
+                <span class="text-white font-cabinet-grotesk text-lg">Tambah gambar</span>
+              </label>
+              <input
+                id="addImg"
+                type="file"
+                name="image"
+                accept="image/*"
+                multiple
+                class="hidden"
+                @change="displayImage"
+              >
+            </div>
+            <!-- judul proyek -->
+            <div class="flex flex-col gap-y-3 mt-4">
+              <label for="judul" class="text-[20px]">Judul Proyek</label>
+              <input
+                id="judul"
+                v-model="form.judul"
+                name="judul"
+                type="text"
+                placeholder="Masukan Judul"
+                class="px-2 py-3 text-[14px] outline-none border rounded-lg tracking-wider font-light"
+              >
+            </div>
+
+            <!-- desc proyek -->
+            <div class="flex flex-col mt-4">
+              <label for="desc" class="text-[20px]">Deskripsi</label>
+              <textarea
+                id="desc"
+                v-model="form.deskripsi"
+                name="desc"
+                placeholder="Masukan Deskripsi"
+                class="px-2 py-3 text-[14px] outline-none border rounded-lg"
+                rows="28"
+                cols="30"
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <!-- show modal for access -->
+      <modal-kelola-akses />
+    </form>
+  </div>
+</template>
+
+<script>
+import { mapState, mapMutations, mapActions } from 'vuex'
+
+export default {
+  name: 'CreateProject',
+  layout: 'CreateProject',
+  data () {
+    return {
+      currentImageIndex: 0,
+      form: {
+        judul: '',
+        deskripsi: ''
+      }
+    }
+  },
+
+  computed: {
+    ...mapState('project', ['selectedImg', 'showPreview']),
+    isFormValid () {
+      const { projectType } = this.$store.state.project.form
+      return (
+        this.selectedImg.length > 0 &&
+        this.form.judul !== '' &&
+        this.form.deskripsi !== '' &&
+        projectType !== ''
+      )
+    }
+  },
+
+  methods: {
+    ...mapMutations('project', [
+      'setSelectedImg',
+      'setShowPreview',
+      'setShowModal',
+      'setJudul',
+      'setDesc'
+    ]),
+    ...mapActions('project', ['postData']),
+
+    // ...mapActions('project', ['postData']),
+
+    displayImage (event) {
+      const files = event.target.files
+      for (let i = 0; i < files.length; i++) {
+        const reader = new FileReader()
+        reader.onload = () => {
+          this.setSelectedImg(reader.result)
+          if (i === files.length - 1) {
+            this.setShowPreview(true)
+          }
+        }
+        reader.readAsDataURL(files[i])
+      }
+    },
+
+    // displayImage (files) {
+    //   for (let i = 0; i < files.length; i++) {
+    //     const file = files[i]
+    //     const reader = new FileReader()
+    //     reader.onload = (event) => {
+    //       const imageUrl = URL.createObjectURL(file) // URL gambar menggunakan URL.createObjectURL
+    //       this.setSelectedImg(imageUrl)
+    //       if (i === files.length - 1) {
+    //         this.setShowPreview(true)
+    //       }
+    //     }
+    //     reader.readAsDataURL(file)
+    //   }
+    // },
+    nextImage () {
+      if (this.currentImageIndex < this.selectedImg.length - 1) {
+        this.currentImageIndex++
+      }
+    },
+    prevImage () {
+      if (this.currentImageIndex > 0) {
+        this.currentImageIndex--
+      }
+    },
+
+    // submit
+    async simpan (event) {
+      event.preventDefault()
+      if (this.isFormValid) {
+        this.setJudul(this.form.judul)
+        this.setDesc(this.form.deskripsi)
+        // ... Do any additional logic or API calls to save the data ...
+      }
+      try {
+        await this.postData() // Mengirimkan data form menggunakan action Vuex
+        // Handling jika posting data berhasil
+      } catch (error) {
+        // Handling jika terjadi kesalahan saat posting data
+        console.error(error)
+      }
+    },
+
+    showAkses () {
+      this.setShowModal(true)
+    }
+  }
+  // Fungsi untuk mengonversi gambar ke format PNG
+}
+</script>
