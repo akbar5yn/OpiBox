7cc6735133c84a76c82177d261ca988de389c4fa
akb4r5yn <akbar5yn.developer@gmail.com>
2023-05-19 14:45:51 +0700

    added annotation created_at using lib moment and passed params to pass data to another page

--- components/mainpage/MyProject.vue
+++ components/mainpage/MyProject.vue
@@ -1,9 +1,9 @@
 <template>
-  <div>
+  <div class="my-project max-w-screen-2xl snap-x overflow-x-scroll">
     <!-- kondisi belum ada project -->
-    <div
+    <section
       v-if="noProject"
-      class="w-full rounded-lg bg-[#6C61E1] py-[59px] flex justify-center items-center"
+      class="no-project w-full rounded-lg bg-[#6C61E1] py-[59px] flex justify-center items-center"
     >
       <div class="flex items-center gap-5">
         <icon-galery-img-folder />
@@ -12,47 +12,122 @@
           <span class="text-xl font-normal">Yuk, buat proyekmu sekarang</span>
         </p>
       </div>
-    </div>
+    </section>
 
     <!-- setelah ada project -->
-    <div
-      v-if="projectExists"
-      class="border rounded-lg bg-white drop-shadow-md w-fit"
-    >
-      <div class="h-[152px] w-[360px] relative overflow-clip">
-        <img
-          src="../../assets/img/unsplash_rd7RBGvs_-4.svg"
-          alt="cover-project"
-          class="object-contain w-full"
-        >
-      </div>
-      <div class="p-[10px] pr-3 flex items-center justify-between">
-        <div class="flex items-center gap-[10px]">
-          <!-- icon team -->
-          <div class="bg-[#E5E5E6] p-4 rounded-full w-fit">
-            <img src="../../assets/img/team.svg" alt="icon-team">
-          </div>
-          <h2 class="font-cabinet-grotesk text-xl font-semibold">
-            Desain Daftar Menu Reato <br>
-            <span class="font-normal text-sm">Di ubah 5 menit yang lalu</span>
-          </h2>
+    <section class="project-exist flex gap-3 scroll-ml-6 snap-start">
+      <div
+        v-for="project in getMyProject"
+        :key="project.id"
+        class="border rounded-lg bg-white drop-shadow-md w-fit snap-start"
+      >
+        <div class="h-[152px] w-[360px] relative overflow-clip">
+          <img
+            :src="project.images[0].image.thumbnail.url"
+            alt="cover-project"
+            class="object-contain w-full"
+          >
         </div>
-        <button>
-          <icon-galery-dots-icon />
-        </button>
+        <div class="p-[10px] pr-3 flex items-center justify-between">
+          <div class="flex items-center gap-[10px]">
+            <!-- icon team -->
+            <div class="bg-[#E5E5E6] p-4 rounded-full w-fit">
+              <img src="../../assets/img/team.svg" alt="icon-team">
+            </div>
+            <h2
+              class="cursor-pointer font-cabinet-grotesk text-xl font-semibold"
+              @click="showProject(project.id)"
+            >
+              {{ project.title }} <br>
+
+              <span class="font-normal text-sm">{{ project.createdAgo }}</span>
+            </h2>
+          </div>
+          <button>
+            <icon-galery-dots-icon />
+          </button>
+        </div>
       </div>
-    </div>
+    </section>
   </div>
 </template>
 
 <script>
+import { mapActions, mapGetters } from 'vuex'
+import moment from 'moment'
+
 export default {
   name: 'MyProject',
   data () {
     return {
       noProject: false,
-      projectExists: true
+      projectExists: false
     }
+  },
+  computed: {
+    ...mapGetters('project', ['getMyProject'])
+  },
+
+  mounted () {
+    this.fetchMyProject()
+  },
+
+  methods: {
+    ...mapActions('project', ['fetchMyProject']),
+
+    showProject (projectId) {
+      // Navigasi ke halaman detail proyek berdasarkan ID
+      this.$router.push(`/project/${projectId}`)
+    },
+    updateProjectStatus () {
+      this.noProject = this.getMyProject.length === 0
+      this.projectExists = this.getMyProject.length > 0
+
+      this.getMyProject.forEach((project) => {
+        const createdTime = moment(project.created_at)
+        const currentTime = moment()
+        const duration = moment.duration(currentTime.diff(createdTime))
+        let createdAgo
+
+        if (duration.asMinutes() < 1) {
+          createdAgo = 'Baru saja dibuat'
+        } else if (duration.asMinutes() < 60) {
+          createdAgo =
+            Math.floor(duration.asMinutes()) === 1
+              ? '1 menit yang lalu'
+              : `${Math.floor(duration.asMinutes())} menit yang lalu`
+        } else if (duration.asHours() < 24) {
+          const hoursDiff = duration.asHours()
+          createdAgo =
+            hoursDiff === 1
+              ? '1 jam yang lalu'
+              : `${Math.floor(hoursDiff)} jam yang lalu`
+        } else {
+          const daysDiff = duration.asDays()
+          createdAgo =
+            daysDiff === 1
+              ? '1 hari yang lalu'
+              : `${Math.floor(daysDiff)} hari yang lalu`
+        }
+
+        project.createdAgo = createdAgo
+      })
+    }
+  },
+  watch: {
+    getMyProject () {
+      this.updateProjectStatus()
+    }
+  },
+
+  created () {
+    this.updateProjectStatus()
   }
 }
 </script>
+
+<style scoped>
+.my-project::-webkit-scrollbar {
+  display: none;
+}
+</style>
