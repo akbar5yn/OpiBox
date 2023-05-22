d4b71a02240aadf40bd38b0b5ba8ca410611f12b
Rinta Sari <rintasari64@gmail.com>
2023-05-23 00:10:49 +0700

    slicing pop up update profile

--- components/ModalBase.vue
+++ components/ModalBase.vue
@@ -1,13 +1,20 @@
 <template>
   <div
     class="fixed inset-0 flex justify-center bg-[#000000] bg-opacity-50 z-50 overflow-y-auto"
-    @click="$emit('close-modal')"
   >
-    <div class="h-fit rounded-xl p-8 relative bg-white self-center">
+    <div
+      class="fixed top-0 bottom-0 left-0 right-0"
+      @click="$emit('close-modal')"
+    />
+    <div
+      class="modal-base w-full h-fit rounded-xl p-8 relative bg-white self-center transition duration-150 ease-in-out"
+      :style="containerCss"
+    >
       <img
         class="w-3 absolute right-4 top-4 cursor-pointer"
         src="../assets/img/close.svg"
         alt="Close Icon"
+        @click="$emit('close-modal')"
       >
       <slot />
     </div>
@@ -15,6 +22,25 @@
 </template>
 <script>
 export default {
-  name: 'ModalBase'
+  name: 'ModalBase',
+  props: {
+    width: {
+      type: String,
+      default: ''
+    }
+  },
+  computed: {
+    containerCss () {
+      return {
+        '--width': this.width + 'px'
+      }
+    }
+  }
 }
 </script>
+
+<style scoped>
+.modal-base {
+  max-width: var(--width);
+}
+</style>
--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -24,7 +24,11 @@
             class="w-[55px] rounded-full"
             :src="detailUser.avatar.image.thumbnail.url"
           >
-          <img src="../../assets/img/editIcon.svg">
+          <img
+            class="cursor-pointer"
+            src="../../assets/img/editIcon.svg"
+            @click="editProfileVisibility = true"
+          >
         </div>
         <span class="font-semibold">{{ detailUser.name }}</span>
         <span class="text-sm font-[400] text-opacity-50">
@@ -38,6 +42,73 @@
         </p>
       </div>
     </div>
+
+    <ModalBase
+      v-if="editProfileVisibility"
+      width="800"
+      @close-modal="editProfileVisibility = false"
+    >
+      <h1 class="text-2xl font-semibold">
+        Ubah Profile
+      </h1>
+      <div class="flex items-center space-x-12 mt-10">
+        <div>
+          <img
+            class="max-w-[120px] w-full rounded-full"
+            :src="detailUser.avatar.image.thumbnail.url"
+          >
+          <button>Ubah foto profile</button>
+        </div>
+        <div class="w-full">
+          <form class="flex flex-col space-y-4" @submit.prevent="changeProfile">
+            <div class="flex flex-col space-y-2">
+              <label>Nama</label>
+              <input
+                v-model="form.name"
+                class="border border-gray-300 p-2 rounded-lg"
+                type="text"
+              >
+              <div
+                v-if="errors.name != ''"
+                class="error-message text-red-500 text-sm"
+              >
+                {{ errors.name }}
+              </div>
+            </div>
+            <div class="flex flex-col space-y-2">
+              <label>Email</label>
+              <input
+                class="border border-gray-300 p-2 rounded-lg bg-gray-200 text-gray-400"
+                type="email"
+                :value="detailUser.email"
+                disabled
+              >
+            </div>
+            <div class="flex flex-col space-y-2">
+              <label>Deskripsi</label>
+              <textarea
+                v-model="form.bio"
+                class="border border-gray-300 p-2 rounded-lg w-full h-[150px]"
+                placeholder="Masukkan deskripsi"
+              />
+              <div
+                v-if="errors.bio != ''"
+                class="error-message text-red-500 text-sm"
+              >
+                {{ errors.bio }}
+              </div>
+            </div>
+            <button
+              class="text-white p-3 w-full max-w-[200px] rounded-lg text-[18px] ml-auto"
+              :disabled="disabled"
+              :class="{ 'bg-[#6C61E1]': !disabled, 'bg-gray-200': disabled }"
+            >
+              Simpan Perubahan
+            </button>
+          </form>
+        </div>
+      </div>
+    </ModalBase>
   </div>
 </template>
 
@@ -50,8 +121,58 @@ export default {
   async asyncData ({ store }) {
     await store.dispatch('profile/getProfile')
   },
+  data () {
+    return {
+      disabled: true,
+      form: {
+        name: this.$store.state.profile.detailUser.name ?? '',
+        bio: this.$store.state.profile.detailUser.bio ?? '',
+        avatar:
+          this.$store.state.profile.detailUser.avatar.image.thumbnail.url ?? ''
+      },
+      errors: {
+        name: '',
+        bio: '',
+        avatar: ''
+      },
+      editProfileVisibility: false
+    }
+  },
   computed: {
     ...mapState('profile', ['detailUser'])
+  },
+  watch: {
+    form: {
+      deep: true,
+      handler () {
+        this.disabled = !(this.validateUsername() && this.validateFullName())
+      }
+    },
+    'form.name': {
+      handler () {
+        this.errors.name = !this.validateUsername()
+          ? 'Nama harus diisi'
+          : !this.validateFullName()
+              ? 'Nama hanya boleh berisi huruf saja'
+              : ''
+      }
+    }
+  },
+  methods: {
+    changeProfile () {
+      const data = {
+        name: this.form.name,
+        bio: this.form.bio
+      }
+      console.log({ data })
+    },
+    validateUsername () {
+      return this.form.name.length > 0
+    },
+    validateFullName () {
+      const fullNameRegex = /^[a-zA-Z\s]+$/
+      return fullNameRegex.test(this.form.name)
+    }
   }
 }
 </script>
