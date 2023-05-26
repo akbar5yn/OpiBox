a037c253966469bcb2e8e05322c1f59a9617a219
akb4r5yn <akbar5yn.developer@gmail.com>
2023-05-26 07:57:55 +0700

    created componen for display comment user

---
+++ components/ShowComment.vue
@@ -0,0 +1,113 @@
+<template>
+  <div class="h-[100%]">
+    <section
+      v-if="noComment"
+      class="flex h-[100%] flex-col items-center text-center justify-center gap-2"
+    >
+      <p class="font-cabinet-grotesk text-xl font-bold">
+        Belum Ada Komentar
+      </p>
+      <p class="font-cabinet-grotesk">
+        Komentar yang telah ditambahkan akan muncul di sini
+      </p>
+    </section>
+    <section v-else class="like-commenth-[100%] mt-5">
+      <div
+        v-for="(comment, index) in getAllComment"
+        :key="index"
+        class="card border-b-2 py-5 w-full flex flex-col gap-2"
+      >
+        <div class="header flex items-center justify-between">
+          <div class="flex items-center gap-3">
+            <icon-galery-avatar-icon />
+            <div>
+              <p class="font-cabinet-grotesk">
+                {{ comment.name }}
+              </p>
+              <div class="mark flex items-center gap-2">
+                <svg
+                  width="14"
+                  height="16"
+                  viewBox="0 0 14 16"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    clip-rule="evenodd"
+                    d="M2.54445 2.14601C5.00475 -0.314294 8.99369 -0.314295 11.454 2.14601C13.9143 4.60631 13.9143 8.59525 11.454 11.0556L6.99922 15.5103L2.54445 11.0556C0.084143 8.59525 0.084143 4.60631 2.54445 2.14601ZM6.99922 8.40078C7.99333 8.40078 8.79922 7.59489 8.79922 6.60078C8.79922 5.60667 7.99333 4.80078 6.99922 4.80078C6.00511 4.80078 5.19922 5.60667 5.19922 6.60078C5.19922 7.59489 6.00511 8.40078 6.99922 8.40078Z"
+                    fill="#19191B"
+                  />
+                </svg>
+
+                <span class="font-cabinet-grotesk">Gambar 1</span>
+              </div>
+            </div>
+          </div>
+          <icon-galery-dots-icon />
+        </div>
+        <div>
+          <p class="font-cabinet-grotesk">
+            {{ comment.body }}
+          </p>
+        </div>
+        <footer class="font-cabinet-grotesk text-gray-400 font-light">
+          {{ comment.time_ago }}
+        </footer>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import { mapState, mapActions, mapMutations, mapGetters } from 'vuex'
+
+export default {
+  name: 'Project',
+  layout: 'ProjectSession',
+  data () {
+    return {
+      currentImageIndex: 0,
+      fillColor: '#fff',
+      strokeColor: '#19191B',
+      projectIndex: 0,
+      noComment: false,
+      noLike: false,
+      showComment: true,
+      showLike: false,
+      selectedMenu: 'like'
+    }
+  },
+
+  computed: {
+    // get project
+    ...mapState('project', ['projects']),
+    ...mapGetters('project', ['getMyProject']),
+    ...mapGetters('comment', ['getAllComment']),
+    ...mapGetters('comment', ['commentCount']),
+    ...mapState('likes', ['likes', 'isLiked', 'likeCount']),
+    ...mapGetters('likes', ['getLike'])
+  },
+
+  methods: {
+    ...mapActions('project', ['fetchMyProject']),
+    ...mapActions('likes', ['fetchLike', 'likeProject', 'disLike']),
+    ...mapMutations('likes', ['setLike']),
+    ...mapActions('comment', ['fetchComment']),
+
+    updateProjectStatus () {
+      this.noComment = this.getAllComment.length === 0
+    }
+  },
+
+  watch: {
+    getMyProject () {
+      this.updateProjectStatus()
+    }
+  },
+
+  created () {
+    this.updateProjectStatus()
+  }
+}
+</script>
