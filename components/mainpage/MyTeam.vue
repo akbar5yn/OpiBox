9b984a5073de4adbe84d065e72a3e1e931493806
akb4r5yn <akbar5yn.developer@gmail.com>
2023-05-28 10:46:13 +0700

    make a changes to design updates for handle create and show team

--- components/mainpage/MyTeam.vue
+++ components/mainpage/MyTeam.vue
@@ -1,35 +1,22 @@
 <template>
-  <div>
-    <div class="section2 flex flex-col gap-7 p-8">
-      <div class="flex justify-between">
+  <div class="MyTeam h-full">
+    <div class="flex flex-col gap-7 h-full">
+      <div class="flex justify-between px-8 mt-8">
         <h1 class="text-[20px] font-[590] font-cabinet-grotesk">
           Tim Saya
         </h1>
-        <button
-          class="transition-all duration-200"
-          :class="{ ['-rotate-180']: teamVisibility }"
-          @click="showTeam"
-        >
-          <icon-galery-arrow-icon />
-        </button>
+        <NuxtLink to="/mainpage/createTeam" class="">
+          <icon-galery-add-team />
+        </NuxtLink>
       </div>
-      <div v-if="teamVisibility">
-        <ul class="flex flex-col gap-7">
-          <!-- <li v-for="team in getTeams" :key="team.id">
-            <NuxtLink
-              :to="`/teams/${team.name}`"
-              active-class="bg-[#EBEAFB]"
-              class="flex items-center gap-3 font-cabinet-grotesk"
-            >
-              <div class="bg-[#6C61E1] w-4 h-4 rounded-full" />
-              {{ team.name }}
-            </NuxtLink>
-          </li> -->
+
+      <div class="w-full h-full overflow-y-scroll">
+        <ul class="flex w-full flex-col gap-2 h-80">
           <li v-for="team in getTeamsByInv" :key="team.id">
             <NuxtLink
               :to="`/teams/${team.team_name}`"
               active-class="bg-[#EBEAFB]"
-              class="flex items-center gap-3 font-cabinet-grotesk"
+              class="flex items-center gap-3 font-cabinet-grotesk px-8 py-2"
             >
               <div class="bg-[#6C61E1] w-4 h-4 rounded-full" />
               {{ team.team_name }}
@@ -38,14 +25,6 @@
         </ul>
       </div>
     </div>
-    <NuxtLink
-      to="/mainpage/createTeam"
-      class="flex items-center gap-3 py-3 px-8 font-cabinet-grotesk"
-      active-class="bg-[#EBEAFB]"
-    >
-      <icon-galery-add-team />
-      <p>Buat Tim Baru</p>
-    </NuxtLink>
   </div>
 </template>
 
@@ -55,7 +34,7 @@ export default {
   name: 'MyTeam',
   data () {
     return {
-      teamVisibility: false
+      // teamVisibility: false
     }
   },
 
@@ -68,10 +47,10 @@ export default {
   },
 
   mounted () {
+    this.invTeams()
     // const createdBy = this.$auth.user.id
     // this.fetchTeams()
     // const invMe = this.$auth.user
-    this.invTeams()
     // const teamId = 98 // Set the team ID here
     // this.fetchTeamDetails(teamId)
     // this.timer = setInterval(() => {
@@ -79,16 +58,12 @@ export default {
     // }, 1000)
   },
 
-  beforeDestroy () {
-    clearInterval(this.timer) // Membersihkan timer saat komponen dihancurkan
-  },
+  // beforeDestroy () {
+  //   clearInterval(this.timer) // Membersihkan timer saat komponen dihancurkan
+  // },
 
   methods: {
-    ...mapActions('teams', ['fetchTeams', 'invTeams']),
-
-    showTeam () {
-      this.teamVisibility = !this.teamVisibility
-    }
+    ...mapActions('teams', ['fetchTeams', 'invTeams'])
   }
 }
 </script>
