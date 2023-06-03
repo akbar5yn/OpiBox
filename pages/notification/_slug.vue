c7355d01fd6a6fa33eea3bace905b987c5d22501
akb4r5yn <akbar5yn.developer@gmail.com>
2023-06-03 13:20:42 +0700

    rename file index on notification page become _slug

---
+++ pages/notification/_slug.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <header>
+      <h1 class="py-[36px] px-[28px] font-cabinet-grotesk text-3xl font-medium">
+        Notifikasi
+      </h1>
+      <div class="grid grid-cols-2 text-center px-7">
+        <nuxt-link
+          to="/notification/"
+          class="py-[20px] font-cabinet-grotesk text-2xl text-[#95959D]"
+          :class="{
+            'border-b-2 border-b-[#6C61E1] text-cabinet-grotesk text-black':
+              $route.path === '/notification/'
+          }"
+        >
+          Info
+        </nuxt-link>
+        <nuxt-link
+          to="/notification/invitations"
+          class="py-[20px] font-cabinet-grotesk text-2xl text-[#95959D]"
+          :class="{
+            'border-b-2 border-b-[#6C61E1] text-cabinet-grotesk text-black':
+              $route.path === '/notification/invitations'
+          }"
+        >
+          Undangan
+        </nuxt-link>
+      </div>
+    </header>
+
+    <!-- Notification undangan -->
+    <main v-if="$route.params.slug">
+      <div>
+        <h1>Undangan</h1>
+      </div>
+    </main>
+
+    <!-- Notificatin -->
+    <main v-else>
+      <!-- <ShowInformation v-if="selectedNotification === 'info'" /> -->
+      <InviTation />
+    </main>
+  </div>
+</template>
+<script>
+export default {
+  name: 'NotiFication',
+  layout: 'MainPage',
+
+  data () {
+    return {}
+  },
+
+  methods: {}
+}
+</script>
