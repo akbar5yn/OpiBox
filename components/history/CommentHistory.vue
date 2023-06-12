8e967c57149d5888e9a8fb4f7c5f365baa69adb3
akb4r5yn <akbar5yn.developer@gmail.com>
2023-06-13 04:23:53 +0700

    create a component to hold the comment history view

---
+++ components/history/CommentHistory.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="flex flex-col gap-5 pt-5 px-[51px]">
+    <div
+      v-for="comment in getCommentHistory"
+      :key="comment.id"
+      class="relative cursor-pointer border-b p-5"
+    >
+      <div class="flex justify-between items-center font-cabinet-grotesk">
+        <h1 class="text-xl font-medium">
+          {{ comment.project_name }}
+        </h1>
+        <p class="text-[#B0B0B5] text-sm">
+          {{ comment.time_ago }}
+        </p>
+      </div>
+      <p class="text-sm mt-[10px] font-normal">
+        {{ comment.body }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex'
+export default {
+  name: 'LikeHistory',
+
+  mounted () {
+    this.fetchCommentHistory()
+    // this.getHistory()
+  },
+
+  computed: {
+    ...mapGetters('comment', ['getCommentHistory'])
+  },
+
+  methods: {
+    ...mapActions('comment', ['fetchCommentHistory'])
+  }
+}
+</script>
