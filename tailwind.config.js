50dc7ece98e1adcf04967bf67a9dfa813aeacec0
akb4r5yn <akbar5yn.developer@gmail.com>
2023-06-15 10:05:39 +0700

    added breakpoint for handle responsive web design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./components/**/*.{js,vue,ts}",
-    "./layouts/**/*.vue",
-    "./pages/**/*.vue",
-    "./plugins/**/*.{js,ts}",
-    "./nuxt.config.{js,ts}",
+    './components/**/*.{js,vue,ts}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './nuxt.config.{js,ts}'
   ],
   theme: {
     extend: {
       fontFamily: {
-        'open-sans': ['Open Sans', 'sans-serif'],
+        'open-sans': ['Open Sans', 'sans-serif']
       },
       screens: {
-        '3xl': '1920px', // tambahkan breakpoint 3xl
-      },
-    },
+        mobile: '360px',
+        tablet: '768px',
+        laptop: '1024px',
+        xl: '1280px',
+        '2x': '1536px',
+        '3xl': '1920px' // tambahkan breakpoint 3xl
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
-
