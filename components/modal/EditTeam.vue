c2230410aed6b9cd9b179296ca83578fb3742dbf
akb4r5yn <akbar5yn.developer@gmail.com>
2023-06-21 20:04:59 +0700

    changed the icon component call to an svg

--- components/modal/EditTeam.vue
+++ components/modal/EditTeam.vue
@@ -10,7 +10,19 @@
             </h1>
           </div>
           <button class="absolute right-5" @click="closeModal">
-            <icon-galery-close-bar />
+            <svg
+              class="w-3 laptop:w-5"
+              viewBox="0 0 14 14"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M1 13L13 1M1 1L13 13"
+                stroke="#151B1E"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
           </button>
         </div>
 
