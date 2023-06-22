628cfa27ac76c061c864779e9535f1be03954a9f
akb4r5yn <akbar5yn.developer@gmail.com>
2023-06-23 03:53:46 +0700

    add different colors when on different devices

--- components/icon-galery/SliderIcon.vue
+++ components/icon-galery/SliderIcon.vue
@@ -1,5 +1,6 @@
 <template>
   <svg
+    class="icon"
     width="20"
     height="16"
     viewBox="0 0 20 16"
@@ -8,7 +9,6 @@
   >
     <path
       d="M12 1L19 8M19 8L12 15M19 8L1 8"
-      stroke="#19191B"
       stroke-width="2"
       stroke-linecap="round"
       stroke-linejoin="round"
@@ -21,3 +21,27 @@ export default {
   name: 'SliderIcon'
 }
 </script>
+
+<style scoped>
+.icon path {
+  stroke: #19191b; /* Warna stroke default */
+}
+
+@media (max-width: 767px) {
+  .icon path {
+    stroke: white; /* Warna stroke saat ukuran layar <= 767px */
+  }
+}
+
+@media (min-width: 768px) and (max-width: 1023px) {
+  .icon path {
+    stroke: black; /* Warna stroke saat ukuran layar antara 768px dan 1023px */
+  }
+}
+
+@media (min-width: 1024px) {
+  .icon path {
+    stroke: black; /* Warna stroke saat ukuran layar >= 1024px */
+  }
+}
+</style>
